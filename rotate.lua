08dfb961b1939ed719c7293dde7a7a2d3ef89448
ClaraBing <liubb16@connect.hku.hk>
2017-05-29 11:41:39 +0800

    pad + rorate

--- rotate.lua
+++ rotate.lua
@@ -4,21 +4,20 @@ local function rotate(fname, theta)
     local img = image.load(fname, 3, 'float')
     local w, h = img:size(3), img:size(2)
     local side = w*math.sin(theta) + math.abs(h*math.cos(theta))
+    local full_side = math.sqrt(2) * side
     local img_mean = img:mean(2):mean(3):reshape(3)
-    print('img mean:')
-    print(img_mean)
-    print('img size:', h, w)
-    print('padded size:', side)
-    local pad_img = torch.FloatTensor(3, side, side)
+    -- Padding w/ img mean
+    local pad_img = torch.FloatTensor(3, full_side, full_side)
     pad_img[1] = img_mean[1]
     pad_img[2] = img_mean[2]
     pad_img[3] = img_mean[3]
-    local w_start, h_start = torch.floor((side-w)/2), torch.floor((side-h)/2)
-    local w_end, h_end =  torch.floor((side+w)/2), torch.floor((side+h)/2)
-    print(w_start, h_start)
-    print(w_end, h_end)
+    local w_start, h_start = torch.floor((full_side-w)/2), torch.floor((full_side-h)/2)
+    local w_end, h_end =  torch.floor((full_side+w)/2), torch.floor((full_side+h)/2)
     pad_img[{{1,3}, {h_start,h_end-1}, {w_start,w_end-1}}] = img
     local rotated = image.rotate(pad_img, theta, 'bilinear')
+    -- Crop img only
+    local crop_start, crop_end = torch.floor((full_side-side)/2), torch.floor((full_side+side)/2)
+    rotated = image.crop(rotated, crop_start, crop_start, crop_end, crop_end) -- Note: the last 2 indices are non-inclusive
     image.save('out.jpg', rotated)
     return
 end
