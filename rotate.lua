78bfd5c12e392d9994449fd1af15a9a703035f51
ClaraBing <liubb16@connect.hku.hk>
2017-06-04 12:49:49 +0800

    random sample

--- rotate.lua
+++ rotate.lua
@@ -1,16 +1,18 @@
 require 'image'
 
-local function rotate(fname, outname, theta)
-    local img = image.load(fname, 3, 'float')
+local function rotate(img, theta)
     local w, h = img:size(3), img:size(2)
+    local img_mean = img:mean(2):mean(3):reshape(3)
+
+    -- Find output img size
     local side = math.max(math.abs(w*math.sin(theta)) + math.abs(h*math.cos(theta)), math.abs(w*math.cos(theta)) + math.abs(h*math.sin(theta)))
     local full_side = math.sqrt(2) * side
-    local img_mean = img:mean(2):mean(3):reshape(3)
     -- Padding w/ img mean
     local pad_img = torch.FloatTensor(3, full_side, full_side)
     pad_img[1] = img_mean[1]
     pad_img[2] = img_mean[2]
     pad_img[3] = img_mean[3]
+
     local w_start, h_start = torch.floor((full_side-w)/2), torch.floor((full_side-h)/2)
     local w_end, h_end =  torch.floor((full_side+w)/2), torch.floor((full_side+h)/2)
     pad_img[{{1,3}, {h_start,h_end-1}, {w_start,w_end-1}}] = img
@@ -19,17 +21,20 @@ local function rotate(fname, outname, theta)
     -- Crop img only
     local crop_start, crop_end = torch.floor((full_side-side)/2), torch.floor((full_side+side)/2)
     rotated = image.crop(rotated, crop_start, crop_start, crop_end, crop_end) -- Note: the last 2 indices are non-inclusive
-    -- Save to file
-    image.save(outname, rotated)
-    return
+    return rotated
 end
 
-rotate('000000.JPEG', '000000_rotated1.JPEG', 1*math.pi/4)
-rotate('000000.JPEG', '000000_rotated2.JPEG', 2*math.pi/4)
-rotate('000000.JPEG', '000000_rotated3.JPEG', 3*math.pi/4)
-rotate('000000.JPEG', '000000_rotated4.JPEG', 4*math.pi/4)
-rotate('000000.JPEG', '000000_rotated5.JPEG', 5*math.pi/4)
-rotate('000000.JPEG', '000000_rotated6.JPEG', 6*math.pi/4)
-rotate('000000.JPEG', '000000_rotated7.JPEG', 7*math.pi/4)
-rotate('000000.JPEG', '000000_rotated8.JPEG', 8*math.pi/4)
-rotate('000000.JPEG', '000000_rotated_neg.JPEG', -math.pi/4)
+local function sampleImg(img, interval)
+    -- A wrapper for 'rotate'
+    -- Input: img: 3*h*w matrix / interval: angle in radius
+    local theta = interval * torch.random(-math.pi/(2*interval), math.pi/(2*interval))
+    return rotate(img, theta)
+end
+
+local img = image.load('000000.JPEG')
+for i=4,12,4 do
+    for iter = 1,4 do
+        rotated = sampleImg(img, math.pi/i)
+        image.save('rand_'..i..'_'..iter..'.jpg', rotated)
+    end
+end
